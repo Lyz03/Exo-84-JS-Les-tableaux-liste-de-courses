139224034e74b20c9c0ff57c6ee9c51ff57c2258
Lyz03 <lizoe@netcourrier.com>
2021-11-26 11:15:25 +0100

    exo 84

--- exercice1.js
+++ exercice1.js
@@ -11,11 +11,39 @@ let articles = [
 ];
 
 // Votre code à partir d'ici :
+let ul = document.getElementById('listeCourse');
+let input = document.getElementById('addItems');
+let content = document.getElementById('tabContent');
 
+content.innerText = articles.join(', ');
 
+/*
+* create li for each value in the array
+* add a delete button
+ */
+function addLi(value) {
+    let li = document.createElement('li');
+    li.innerText = value;
+    ul.appendChild(li);
+    let a = document.createElement('a');
+    a.innerText = ' supprimer';
+    a.style.color = 'red';
+    li.appendChild(a);
+    a.addEventListener("click", function () {
+        li.remove()
+        a.remove();
+    });
+}
 
+for (let value of articles) {
+    addLi(value);
+}
 
-
+document.getElementById('submit').addEventListener("click", function () {
+    addLi(input.value);
+    articles.push(input.value);
+    content.innerText = articles.join(', ');
+});
 
 
 
